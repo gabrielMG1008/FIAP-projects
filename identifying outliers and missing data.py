52038987640cc78f5881f22dca7b0fcd956d805b
Gabriel Matheus <gabrielmg1008@gmail.com>
2025-09-25 21:23:32 -0300

    Update identifying outliers and missing data.py

--- identifying outliers and missing data.py
+++ identifying outliers and missing data.py
@@ -1,49 +1,57 @@
+
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
+import seaborn as sns
 from sklearn.preprocessing import scale
+from sklearn.covariance import EmpiricalCovariance, MinCovDet
+import scipy as sy
+
 
 # chamando os arquivos
-planilha1 = pd.read_csv("vendas_linha_2019.csv", sep=";", encoding="latin1", decimal= ',')
-planilha2 = pd.read_csv("vendas_linha_2020.csv", sep=";", encoding="latin1", decimal= ',')
-planilha3 = pd.read_csv("vendas_linha_2021.csv", sep=";", encoding="latin1", decimal= ',')
-planilha4 = pd.read_csv("vendas_linha_2022.csv", sep=";", encoding="latin1", decimal= ',')
+planilha1 = pd.read_csv("C:\\Users\\gabri\\Downloads\\vendas_linha_petshop_2019.csv", sep=";", encoding="latin1", decimal= ',')
+planilha2 = pd.read_csv("C:\\Users\\gabri\\Downloads\\vendas_linha_petshop_2020.csv", sep=";", encoding="latin1", decimal= ',')
+planilha3 = pd.read_csv("C:\\Users\\gabri\\Downloads\\vendas_linha_petshop_2021.csv", sep=";", encoding="latin1", decimal= ',')
+planilha4 = pd.read_csv("C:\\Users\\gabri\\Downloads\\vendas_linha_petshop_2022.csv", sep=";", encoding="latin1", decimal= ',')
 
 dados = pd.concat([planilha1, planilha2, planilha3, planilha4], ignore_index= True)
 
 # vendo as informações dos CSVs
 print(dados.info())
 
-# procurando valores nulos ou missings
-# contando linhas
-missings_por_linha = pd.DataFrame({'cod_pedido': dados['cod_pedido'].tolist(),
-                                   'n_missings': dados.isna().sum(axis=1).tolist()})
-n_colunas = dados.shape[1] - 1
-# verificando o resultado
-print(missings_por_linha.assign(perc_missings=missings_por_linha['n_missings'] / n_colunas) \
-      .sort_values('perc_missings', ascending=False) \
-      .head(10))
-
-# contando colunas
-missings_por_coluna = dados.isna().sum()
-missings_por_coluna = pd.DataFrame( missings_por_coluna,
-                                    columns=['n']) \
-                                    .reset_index() \
-                                    .rename(columns={'index': 'variaveis'})
-n_linhas = dados.shape[0]
-
-# verificando o resultado
-print(missings_por_coluna \
-    .assign( perc_missings = missings_por_coluna["n"] / n_linhas) \
-    .sort_values("n", ascending= False))
-
-
 # procurando valores outliers
 #pd.set_option("display.max_columns", None) # mostra todas as colunas
 dados['outliers'] = scale(dados['cod_pedido'])
 print(dados.sort_values('outliers', ascending= False))
 
-dados[['lucro_liquido','valor_total_bruto']].boxplot()
-plt.show()
+dados[['valor','valor_total_bruto']].boxplot()
+#plt.show()
 
-dados.plot.scatter(x = 'lucro_liquido',y = 'valor_total_bruto')
-plt.show()
+dados.plot.scatter(x = 'valor',y = 'valor_total_bruto')
+#plt.show()
+
+#desvio padrão comum
+print(
+dados.groupby('valor').agg(
+    desvio_medio = pd.NamedAgg(column='valor_total_bruto', aggfunc='std')
+).reset_index()
+)
+
+#intervalo interquartil (IQR)
+print(
+    dados.groupby('valor')['valor_total_bruto'].apply(
+        lambda x: x.quantile([0.05,0.25,0.5,0.75,0.95])
+    ).to_frame()
+    .reset_index().rename(columns={'level_1':'percentil'}
+    )
+)
+
+#MAD
+def mad(x):
+    return np.median(np.abs(x - np.median(x)))
+
+mad_por_valor = dados.groupby("valor").agg(
+    mad_valor_total_bruto=pd.NamedAgg(column="valor_total_bruto", aggfunc=mad)
+).reset_index()
+
+print(mad_por_valor)
